25c6295ba43e878e0e5c7142a88f4311f0ba83c7
Colactic <Christian.S.Bibby@gmail.com>
2022-04-18 23:11:45 +0200

    Added tests
    -Added "REGISTRERING FOR FREJA" test
    -Added "MIN ANSOKAN GODKANDES INTE" test
    -Added "GENERELLA FRAGOR" test
    -Added "DELAD KONTROLL" test
    -Added "COVIDBEVIS I FREJA" test
    -Added "ORGANISATIONS ID" test
    -Added "INTEGRITET" test

---
+++ cypress/integration/FrejaIDTest/FrejaID_Tests.js
@@ -0,0 +1,71 @@
+describe('Test the "REGISTRERING FOR FREJA" button', () => {
+    it('Click the "REGISTRERING FOR FREJA" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(0).click()
+        cy.get('div.accordian.fusion-accordian').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/freja-eid-plus-sv/')
+    })
+})
+
+describe('Test the "MIN ANSOKAN GODKANDES INTE" button', () => {
+    it('Click the Home button and check the URL', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(1).click()
+        cy.get('div.fusion-fullwidth.fullwidth-box.fusion-builder-row-2.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling').should('exist').and('be.visible')
+        cy.get('div.fusion-fullwidth.fullwidth-box.fusion-builder-row-3.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling').should('exist').and('be.visible')
+        cy.get('div.fusion-fullwidth.fullwidth-box.fusion-builder-row-4.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/hjalp-med-id-handling/')
+    })
+})
+
+describe('Test the "GENERELLA FRAGOR" button', () => {
+    it('Click the "GENERELLA FRAGOR" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(2).click()
+        cy.get('div.accordian.fusion-accordian').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/om-freja-eid/')
+    })
+})
+
+describe('Test the "DELAD KONTROLL" button', () => {
+    it('Click the "DELAD KONTROLL" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(3).click()
+        cy.get('div.accordian.fusion-accordian.feid-text-dark').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/delad-kontroll/')
+    })
+})
+
+describe('Test the "COVIDBEVIS I FREJA" button', () => {
+    it('Click the "COVIDBEVIS I FREJA" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(4).click()
+        cy.get('div.accordian.fusion-accordian.feid-text-dark').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/covidbevis/')
+    })
+})
+
+describe('Test the "ORGANISATIONS ID" button', () => {
+    it('Click the "ORGANISATIONS ID" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(5).click()
+        cy.get('div.accordian.fusion-accordian.feid-text-dark').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/organisations-eid/')
+    })
+})
+
+describe('Test the "INTEGRITET" button', () => {
+    it('Click the "INTEGRITET" button and make sure the panel is visible and url is correct', () => {
+        cy.visit('https://frejaeid.com/vanliga-fragor/')
+        cy.scrollTo(0, 500)
+        cy.get('div.fusion-align-block').eq(6).click()
+        cy.get('div.accordian.fusion-accordian').should('exist').and('be.visible')
+        cy.url().should('include', '/vanliga-fragor/gdpr-sv/')
+    })
+})
